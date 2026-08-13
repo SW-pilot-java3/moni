a6ba3e60762504a1546ddcc011964f352891d0e9
Doritosch <als000825@naver.com>
2026-08-13 11:39:24 +0900

    feat: Instance 요청 DTO에 IPv4 형식 검증 추가

--- api/src/main/java/com/moni/api/domain/instance/dto/InstanceCreateRequest.java
+++ api/src/main/java/com/moni/api/domain/instance/dto/InstanceCreateRequest.java
@@ -1,9 +1,10 @@
 package com.moni.api.domain.instance.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.IpAddress;
 
 public record InstanceCreateRequest(
         @NotBlank String name,
-        @NotBlank String ip
+        @NotBlank @IpAddress(type = IpAddress.Type.IPv4) String ip
 ) {
 }
