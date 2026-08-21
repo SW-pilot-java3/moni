b602fa00a6da71e28d02dacdc35bb0538c99df49
Doritosch <als000825@naver.com>
2026-08-21 20:35:08 +0900

    feat: 인스턴스 목록 조회 기능 추가

--- api/src/main/java/com/moni/api/domain/instance/repository/InstanceRepository.java
+++ api/src/main/java/com/moni/api/domain/instance/repository/InstanceRepository.java
@@ -1,7 +1,10 @@
 package com.moni.api.domain.instance.repository;
 
 import com.moni.api.domain.instance.entity.Instance;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InstanceRepository extends JpaRepository<Instance, Long> {
+
+    List<Instance> findAllByUserIdOrderByCreatedAtDesc(Long userId);
 }
